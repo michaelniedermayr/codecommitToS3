3d3688d3323276de84508258242627970d8da26d
michael <1@97p.xyz>
2019-07-22 19:07:34 +0200

    Upload only new or modified files to S3 bucket

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # codecommitToS3
 
-Tutorial: Code hosted on AWS CodeCommit Git-repo is deployed to AWS S3-bucket by an AWS Lambda function.
+###Update
+If the environmental variable 'branch' is defined, all files of that branch are uploaded to the S3 bucket
+every time a new commit is pushed.
+
+If 'branch' is **NOT** set, HEAD is used to define which files will be uploaded. It will be stored as an 
+[SSM parameter](https://docs.aws.amazon.com/systems-manager/latest/userguide/systems-manager-parameter-store.html) 
+afterwards. This way only modified or new files will be uploaded to the S3 bucket the next time. 
+
+**IMPORTANT**: The Lambda function requires the execution role 'AmazonSSMFullAccess' to access SSM parameters.
+
+Thx to [handk85](https://github.com/handk85) for this improvement.
+
+###Tutorial
+Code hosted on AWS CodeCommit Git-repo is deployed to AWS S3-bucket by an AWS Lambda function.
 
 Find more information here: https://medium.com/@michael.niedermayr/using-aws-codecommit-and-lambda-for-automatic-code-deployment-to-s3-bucket-b35aa83d029b
--- lambda_function.py
+++ lambda_function.py
@@ -3,23 +3,36 @@ import os
 import mimetypes
 
 
-# basically returns a list of a all files in the branch
-def get_blob_list(codecommit, repository, branch):
-    response = codecommit.get_differences(
-        repositoryName=repository,
-        afterCommitSpecifier=branch,
-    )
+# returns a list of the files in the branch or commit
+def get_blob_list(codecommit, repository, afterCommitSpecifier, beforeCommitSpecifier=None, ):
+    args = {'repositoryName': repository, 'afterCommitSpecifier': afterCommitSpecifier}
+
+    if beforeCommitSpecifier:
+        args['beforeCommitSpecifier'] = beforeCommitSpecifier
+
+    response = codecommit.get_differences(**args)
 
     blob_list = [difference['afterBlob'] for difference in response['differences']]
     while 'nextToken' in response:
-        response = codecommit.get_differences(
-            repositoryName=repository,
-            afterCommitSpecifier=branch,
-            nextToken=response['nextToken']
-        )
+        args['nextToken'] = response['nextToken']
+        response = codecommit.get_differences(**args)
         blob_list += [difference['afterBlob'] for difference in response['differences']]
 
     return blob_list
+
+
+def upload_files_to_s3_bucket(bucket, codecommit, repository_name, blob_list):
+    # reads each file in the branch and uploads it to the s3 bucket
+    for blob in blob_list:
+        path = blob['path']
+        content = (codecommit.get_blob(repositoryName=repository_name, blobId=blob['blobId']))['content']
+
+        # we have to guess the mime content-type of the files and provide it to S3 since S3 cannot do this on its own.
+        content_type = mimetypes.guess_type(path)[0]
+        if content_type is not None:
+            bucket.put_object(Body=(content), Key=path, ContentType=content_type)
+        else:
+            bucket.put_object(Body=(content), Key=path)
 
 
 # lambda-function
@@ -30,12 +43,11 @@ def get_blob_list(codecommit, repository, branch):
 #     s3BucketName
 #     codecommitRegion
 #     repository
-#     branch
 #
 # TIME OUT: 1 min
 #
 # EXECUTION ROLE
-#     lambda-codecommit-s3-execution-role (permissions: AWSCodeCommitReadOnly, AWSLambdaExecute)
+#     lambda-codecommit-s3-execution-role (permissions: AWSCodeCommitReadOnly, AWSLambdaExecute, AmazonSSMFullAccess)
 #
 def lambda_handler(event, context):
 
@@ -46,14 +58,33 @@ def lambda_handler(event, context):
     codecommit = boto3.client('codecommit', region_name=os.environ['codecommitRegion'])
     repository_name = os.environ['repository']
 
-    # reads each file in the branch and uploads it to the s3 bucket
-    for blob in get_blob_list(codecommit, repository_name, os.environ['branch']):
-        path = blob['path']
-        content = (codecommit.get_blob(repositoryName=repository_name, blobId=blob['blobId']))['content']
+    # If the environmental variable 'branch' is defined, all files of that branch are uploaded to the S3 bucket
+    # every time a new commit is pushed.
+    # If 'branch' is NOT set, HEAD is used to define which files will be uploaded. It will be stored as an
+    # SSM parameter afterwards. This way only modified or new files will be uploaded to the S3 bucket the next time.
+    if 'branch' in os.environ:
+        blob_list = get_blob_list(codecommit, repository_name, os.environ['branch'])
+        upload_files_to_s3_bucket(bucket, codecommit, repository_name, blob_list)
 
-        # we have to guess the mime content-type of the files and provide it to S3 since S3 cannot do this on its own.
-        content_type = mimetypes.guess_type(path)[0]
-        if content_type is not None:
-            bucket.put_object(Body=(content), Key=path, ContentType=content_type)
-        else:
-            bucket.put_object(Body=(content), Key=path)
+    else:
+        # Current HEAD SHA-1 id
+        head = event['Records'][0]['codecommit']['references'][0]['commit']
+
+        ssm_client = boto3.client('ssm')
+        ssm_param_name = repository_name + '_beforeCommitSpecifier'
+
+        # Check if beforeCommitSpecifier is already set. If not, blob_list retrieves a list of all files.
+        try:
+            # Previous HEAD SHA-1 id (i.e. the commit right before the HEAD when this function is called)
+            last_head = ssm_client.get_parameter(Name=ssm_param_name)['Parameter']['Value']
+
+        except ssm_client.exceptions.ParameterNotFound:
+            last_head = None
+
+        blob_list = get_blob_list(codecommit, repository_name, head, last_head)
+
+        # upload files to s3 bucket
+        upload_files_to_s3_bucket(bucket, codecommit, repository_name, blob_list)
+
+        # store current head as new beforeCommitSpecifier in ssm
+        ssm_client.put_parameter(Name=ssm_param_name, Type='String', Value=head,Overwrite=True)
